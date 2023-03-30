509154a6a39fe6d00a98bc5c6589206ef2d5bb08
Justin Kay <14783039+Justinkay0@users.noreply.github.com>
2023-03-30 23:11:58 +0800

    Pomodoro Timer 2 : create UI for timer

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import tkinter as tk
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -9,10 +10,16 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+pomo = 0
+countdown = 0
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset():
+    pass
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
@@ -31,7 +38,22 @@ canvas.create_image(100, 112, image=tomato)
 # Create text onto tomato
 canvas.create_text(110, 140, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
 
+# buttons
+start = tk.Button(text='Start')
+start.grid(row=2, column=0)
+reset = tk.Button(text='Reset')
+reset.grid(row=2, column=2)
+
+# timer text
+timer_label = tk.Label(text='Timer', font=(FONT_NAME, 45, 'normal'), fg=GREEN, background=YELLOW, highlightthickness=0)
+timer_label.grid(row=0, column=1)
+
+# checkmarks
+checkmarks = tk.Label(text='✓', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25, 'normal'))
+checkmarks.grid(column=1, row=3)
+
 # Place image on window
-canvas.pack()
+canvas.grid(row=1, column=1)
+
 
 window.mainloop()
