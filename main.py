9c51fe88cfa0e5c3e8d3b0548111c351b89703de
Justin Kay <14783039+Justinkay0@users.noreply.github.com>
2023-03-30 22:54:31 +0800

    Pomodoro Timer 1 : placing tomato and create text onto tomato

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -16,3 +17,21 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = tk.Tk()
+window.title('Pomodoro Timer')
+window.config(padx=100, pady=100, bg=YELLOW)
+
+# Creating canvas
+canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+# create photo image to utilise
+tomato = tk.PhotoImage(file='tomato.png')
+# Place image on canvas
+canvas.create_image(100, 112, image=tomato)
+# Create text onto tomato
+canvas.create_text(110, 140, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+
+# Place image on window
+canvas.pack()
+
+window.mainloop()
