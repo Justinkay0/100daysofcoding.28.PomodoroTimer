627c280e19401b18100ac16cadd2faadd0597aff
Justin Kay <14783039+Justinkay0@users.noreply.github.com>
2023-03-30 23:49:51 +0800

    Pomodoro Timer 3 : counting down with timer and displaying on tomato

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
+import math
 import tkinter as tk
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -20,14 +20,26 @@ def reset():
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    count_down(5)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = math.floor(count % 60)
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count-1)
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = tk.Tk()
 window.title('Pomodoro Timer')
 window.config(padx=100, pady=100, bg=YELLOW)
+
+# Timer, after is in millisecond
 
 # Creating canvas
 canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
@@ -36,13 +48,13 @@ tomato = tk.PhotoImage(file='tomato.png')
 # Place image on canvas
 canvas.create_image(100, 112, image=tomato)
 # Create text onto tomato
-canvas.create_text(110, 140, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(110, 140, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
 
 # buttons
-start = tk.Button(text='Start')
+start = tk.Button(text='Start', command=start_timer)
 start.grid(row=2, column=0)
-reset = tk.Button(text='Reset')
-reset.grid(row=2, column=2)
+reset_button = tk.Button(text='Reset')
+reset_button.grid(row=2, column=2)
 
 # timer text
 timer_label = tk.Label(text='Timer', font=(FONT_NAME, 45, 'normal'), fg=GREEN, background=YELLOW, highlightthickness=0)
